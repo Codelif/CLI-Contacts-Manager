85e4e7ee90634204056662c9157bb8dc1360ffbd
Harsh Sharma <68972644+Codelif@users.noreply.github.com>
2021-03-17 20:22:39 +0530

    Added csv writer

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
+import pandas as pd
 import csv
-with open('addresses.csv', newline='') as csvfile:
-    
+list_inputs = [input()]
+while list_inputs[-1] != "done":
+    list_inputs.append(input())
+list_inputs.pop(-1)
+with open('new.csv', "w+", newline='') as csvfile:
+    csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow(list_inputs)
+    csvfile.close()
